7b4075e3cd25b7cc9c291d63a48a79e0872be9d2
Selena Deckelmann <selenamarie@gmail.com>
2013-05-01 16:32:52 -0700

    adding lots of exercises

--- list_comprehensions/demo.py
+++ list_comprehensions/demo.py
@@ -1,8 +1,7 @@
-
 import csv
 import json as simplejson
 import argparse
-
+import re
 
 
 def transform_dict(header, row):
@@ -24,6 +23,55 @@ def descriptions(header, arte):
     for d in descriptions:
         print d
 
+def artists(header, arte):
+    # Exercise: Return only artists
+    print "Not implemented"
+    pass
+
+def artists_by_medium(header, arte):
+    # Exercise: Return only artists
+    print "Not implemented"
+    pass
+
+
+def locations(header, arte):
+    # Exercise: Return only locations
+    print "Not implemented"
+    pass
+
+def medium(header, arte):
+    # Exercise: Return only mediums
+    mediums = [ transform_dict(header, row)['medium'] for row in arte ]
+    # Exercise: sort and only print unique mediums
+    # hint: sorted and set
+    for m in mediums:
+        print m
+
+def medium_by_type(header, arte, medium_type):
+    # Exercise: Return mediums of a certain type
+    mediums = [ transform_dict(header, row)['medium'] for row in arte ]
+    regex = re.compile( '(' + medium_type + ')' )
+    for m in mediums:
+        match = re.search(regex, m)
+        if match:
+            print m
+
+def artists_by_firstname(header, arte, name):
+    # Exercise: search for artists by first name
+    print "not implemented"
+
+def artists_by_firstname(header, arte, name):
+    # Exercise: search for artists by first name
+    print "not implemented"
+
+def thumbnail(header, arte):
+    # Exercise: download the thumbnails of all the works of art
+    print "not implemented"
+
+def recid(header, arte):
+    # Exercise: strip the recid out of the detail_url
+    print "not implemented"
+
 def as_json(header, arte):
     # Exercise: Return all as JSON
     alljson = [ transform_json(header, row) for row in arte ]
@@ -40,6 +88,12 @@ def demo(args):
             titles(header, arte)
         elif args.descriptions:
             descriptions(header, arte)
+        elif args.artists:
+            artists(header, arte)
+        elif args.medium:
+            medium(header, arte)
+        elif args.medium_type:
+            medium_by_type(header, arte, args.medium_type)
         elif args.asjson:
             as_json(header, arte)
 
@@ -59,7 +113,10 @@ if __name__ == '__main__':
     argparser = argparse.ArgumentParser(parents=[conf_parser])
     argparser.add_argument("--titles", help="print titles of public art", action="store_true", default=False)
     argparser.add_argument("--descriptions", help="print descriptions of public art", action="store_true", default=False)
-    argparser.add_argument("--asjson", help="return file contents at json", action="store", default=None)
+    argparser.add_argument("--artists", help="print artists", action="store_true", default=False)
+    argparser.add_argument("--medium", help="print mediums", action="store_true", default=False)
+    argparser.add_argument("--asjson", help="return file contents at json", action="store_true", default=False)
+    argparser.add_argument("--medium-type", help="print mediums of type TYPE", action="store", default=None)
     argparser.set_defaults(**defaults)
     args = argparser.parse_args(remaining_argv)
 
