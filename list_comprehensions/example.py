ee14e5f8dc3f782870caa88d0386e23f8b44a0a8
Selena Deckelmann <selenamarie@gmail.com>
2013-05-01 17:37:26 -0700

    some links

--- list_comprehensions/example.py
+++ list_comprehensions/example.py
@@ -20,21 +20,24 @@ print eees
 
 
 
-
-
-# As a List comprehension
+# List comprehensions: a concise way to make lists!
+# The same loop, expressed as a List comprehension
 
 eees = [ element for element in mylist if 'e' in element ]
 print eees
 
 
 # Generator expression -- Lazy evaluation!
-eees = ( element for element in mylist if 'e' in element )
 
+eees = ( element for element in mylist if 'e' in element )
 print eees
 
 for e in eees:
     print e
 
 
+# Some resources:
+# http://www.secnetix.de/olli/Python/list_comprehensions.hawk
+# http://stackoverflow.com/questions/2436607/how-to-use-re-match-objects-in-a-list-comprehension
+# http://docs.python.org/2/tutorial/datastructures.html#list-comprehensions
 
