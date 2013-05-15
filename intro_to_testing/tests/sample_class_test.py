dfb560eb44792f57292a33df4adefdf9b99f27d7
Selena Deckelmann <selenamarie@gmail.com>
2013-05-15 16:25:25 -0700

    add skipping

--- intro_to_testing/tests/sample_class_test.py
+++ intro_to_testing/tests/sample_class_test.py
@@ -9,14 +9,17 @@ def test_sample_class():
     test_list = s.mylist()
     assert_equal(test_list, expected_list)
 
+@unittest.skip("skipping myname test")
 def test_myname():
     """ Verify that we return myname """
     pass
 
+@unittest.skip("skipping myname_to_mylist test")
 def test_add_myname_to_mylist():
     """ Verify that we can add myname to mylist """
     pass
 
+@unittest.skip("skipping add_to_mylist test")
 def test_add_to_mylist():
     """ Verify that we can add a new element to the list """
     pass
